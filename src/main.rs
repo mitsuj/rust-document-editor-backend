c5c23dd27c2b383c03189ac196b27a4e874a91ab
Sumit <sumitjcode@gmail.com>
2025-02-19 23:29:45 +0530

    add websocket support with axum and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ use tracing::info;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
+use axum::{
+    extract::ws::{WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+};
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
@@ -16,6 +21,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(root))
         .route("/health", get(health))
+        .route("/ws", get(websocket_handler))
         .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -33,6 +39,35 @@ async fn health() -> (StatusCode, Json<HealthCheckResponse>) {
         status: "ok".to_string(),
     };
     (StatusCode::OK, Json(response))
+}
+
+async fn websocket_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        match msg {
+            Ok(msg) => {
+                if let axum::extract::ws::Message::Text(text) = msg {
+                    println!("Received message: {:?}", text);
+                    if socket
+                        .send(axum::extract::ws::Message::Text(text))
+                        .await
+                        .is_err()
+                    {
+                        println!("Client disconnected");
+                        return;
+                    }
+                }
+            }
+            Err(err) => {
+                println!("Webdsocket error: {:?}", err);
+                return;
+            }
+        }
+    }
+    println!("Client disconnected");
 }
 
 #[derive(Serialize)]
